52f9f7b2cf7a4f08928ac6b122ad70d1e4343c50
Abijith <abijithtrichurr.cs17@rvce.edu.in>
2020-04-16 16:19:45 +0530

    Implemented read from out file and write to .qcc file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 var app = express();
 const bodyparser = require("body-parser");
 
+var fs = require("fs");
+
 app.use(cors());
 
 app.use(bodyparser.json());
@@ -17,4 +19,47 @@ app.get("/", (req, res) => {
 
 app.post("/data", (req, res) => {
   console.log(req.body);
+
+  let inputData = "";
+
+  for (let i = 0; i < req.body.cols.length; i++) {
+    inputData += "[" + req.body.cols[i].toString() + "]";
+  }
+
+  let inputText =
+    "func test { \n" +
+    inputData +
+    "\n}" +
+    "\n\nquReg qr = new quReg[" +
+    req.body.cols[0].length +
+    "]\n" +
+    "setPrecision(4)\n" +
+    "\n\n" +
+    "qr.test()\n" +
+    "qr.Pnz()";
+
+  console.log(inputText);
+
+  fs.writeFile("input.qc", inputText, function (err) {
+    if (err) throw err;
+  });
+
+  //Command to execute the file - ABeer Vaishnav
+
+  let outputData = {
+    probabilities: [],
+  };
+
+  fs.readFile("out.txt", "utf8", function (err, data) {
+    // Display the file content
+    temp = data.split("\n");
+    for (let i = 5; i < Math.pow(2, 2) + 5; i++) {
+      state = parseInt(temp[i][1]);
+      temp2 = temp[i].split("\t");
+      prob = parseFloat(temp2[2]);
+      outputData.probabilities.push([state, prob]);
+    }
+    console.log(outputData);
+    res.send(outputData);
+  });
 });
